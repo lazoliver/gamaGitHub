1b4e2d6fbdd975628590c1059cb489a73fab5eec
lazoliver <wilian.oliveiractt@gmail.com>
2021-08-18 22:25:18 -0300

    master

---
+++ js/pages/home/styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+export const Input = styled.input `
+    border: 1px solid #ddd;
+    height: 2rem;
+    padding: 0 .5rem;
+    border-radius: .25rem 0 0 .25rem;
+`;
