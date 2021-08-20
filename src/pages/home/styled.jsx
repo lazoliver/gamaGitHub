210694f8055d79b09a0d25b59c75be1654cebdd0
lazoliver <wilian.oliveiractt@gmail.com>
2021-08-19 23:56:21 -0300

    Projeto para consulta de repositórios no GitHub

--- src/pages/home/styled.jsx
+++ src/pages/home/styled.jsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    width: 100vw;
+export const HomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     height: 100vh;
+`;
+
+export const Content = styled.div`
+    width: 100vw;
     display: flex;
     align-itens: center;
     justify-content: center;
@@ -22,15 +29,19 @@ export const Input = styled.input`
 `;
 
 export const Button = styled.button`
-    height: 1.5rem;
+    height: 2rem;
     border: 1px solid #000;
     background: #000;
     color: #fff;
     border-radius: 0 .25rem .25rem 0;
      
     &:focus,
-    &:active {
-        outline: none;
-        box-shadow: none;
-    }
+`;
+
+export const ErrorMsg = styled.span`
+    display: block;
+    font-size: 1rem;
+    color: red;
+    font-weight: 600;
+    margin-top: 1rem;
 `;
--- src/pages/repositories/index.jsx
+++ src/pages/repositories/index.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import * as S from './styled'
+import React, { useEffect, useState } from 'react';
+import * as S from './styled';
+import { useHistory } from 'react-router-dom';
  
 export default function Repositories() {
+    const history = useHistory();
+    const [ repositories, setRepositories ] = useState([]);
+
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        if(repositoriesName != null) {   
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else {
+            history.push('/');
+        }
+    }, []);
+
     return (
         <S.Container>    
             <S.Title>Seus Repositórios</S.Title>
             <S.List>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
+                { repositories.map(repository => {
+                    return (
+                        <S.ListItem>Repositório: { repository }</S.ListItem>
+                    )
+                }) }
             </S.List>
+            <S.LinkBack to="/" >Voltar</S.LinkBack>
         </S.Container>    
     );
 }
